2323b86cf60bce9b3045ecfcb49250dfd1e3f644
Luke Phelan <luke.phelan@hotmail.com>
2024-03-11 13:56:08 +1100

    drivers page (#1)
    * drivers page
    
    * lint fix

---
+++ src/app/ui/drivers/table.tsx
@@ -0,0 +1,93 @@
+import { fetchFilteredDrivers } from '@/app/lib/data';
+import { formatDateToLocal } from '@/app/lib/utils';
+import { Driver } from '@/app/lib/definitions';
+
+export function MobileTable({ drivers }: { drivers: Driver[] }) {
+  return (
+    <div className='md:hidden'>
+      {drivers?.map((driver) => (
+        <div
+          key={driver.driverId}
+          className='mb-2 w-full rounded-md bg-white p-4'
+        >
+          <div className='border-b pb-4'>
+            {driver.forename} {driver.surname}
+          </div>
+          <div className='border-b pb-4'>{driver.number || '—'}</div>
+          <div className='border-b pb-4'>{driver.code || '—'}</div>
+          <div className='border-b pb-4'>
+            {formatDateToLocal(driver.dob.toString())}
+          </div>
+          <div className='border-b pb-4'>{driver.nationality}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export function DesktopTable({ drivers }: { drivers: Driver[] }) {
+  return (
+    <table className='hidden min-w-full text-gray-900 md:table'>
+      <thead className='rounded-lg text-left text-sm font-normal'>
+        <tr>
+          <th scope='col' className='px-4 py-5 font-medium'>
+            Driver
+          </th>
+          <th scope='col' className='px-3 py-5 font-medium'>
+            Number
+          </th>
+          <th scope='col' className='px-3 py-5 font-medium'>
+            Code
+          </th>
+          <th scope='col' className='px-3 py-5 font-medium'>
+            DOB
+          </th>
+          <th scope='col' className='px-3 py-5 font-medium'>
+            Nationality
+          </th>
+        </tr>
+      </thead>
+      <tbody className='bg-white'>
+        {drivers?.map((driver) => (
+          <tr
+            key={driver.driverId}
+            className='w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg'
+          >
+            <td className='whitespace-nowrap px-3 py-3'>
+              {driver.forename} {driver.surname}
+            </td>
+            <td className='whitespace-nowrap px-3 py-3'>
+              {driver.number || '—'}
+            </td>
+            <td className='whitespace-nowrap px-3 py-3'>
+              {driver.code || '—'}
+            </td>
+            <td className='whitespace-nowrap px-3 py-3'>
+              {formatDateToLocal(driver.dob.toString())}
+            </td>
+            <td className='whitespace-nowrap px-3 py-3'>
+              {driver.nationality}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default async function DriversTable({
+  query,
+  currentPage,
+}: {
+  query: string;
+  currentPage: number;
+}) {
+  const drivers = await fetchFilteredDrivers(query, currentPage);
+
+  return (
+    <>
+      <MobileTable drivers={drivers} />
+      <DesktopTable drivers={drivers} />
+    </>
+  );
+}
---
+++ src/app/ui/nav-links.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import clsx from 'clsx';
+
+const links = [
+  { name: 'Home', href: '/' },
+  { name: 'Drivers', href: '/drivers' },
+  {},
+  { name: 'Acknowledgements', href: '/acknowledgements' },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {links.map((link, index) => {
+        if (link.name) {
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={clsx(
+                'flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-red-600 hover:text-red-100 md:flex-none md:justify-start md:p-2 md:px-3',
+                {
+                  'bg-slate-100 text-slate-600': pathname === link.href,
+                }
+              )}
+            >
+              <p className='hidden md:block'>{link.name}</p>
+            </Link>
+          );
+        } else {
+          return (
+            <div
+              key={`space-${index}`}
+              className='hidden h-auto w-full grow rounded-md md:block'
+            ></div>
+          );
+        }
+      })}
+    </>
+  );
+}
